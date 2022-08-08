73ca0dca92e237c0dfed16e0911d1dd97fe1c31c
awadh <awadh.hassan@student.moringaschool.com>
2022-08-08 14:47:02 +0300

    almost

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+
+const newHeader = document.createnewHeader("div")
+
+document.body.append(div)
+
+const h1 =document.getElementById("h1");
+newHeader.append(h1);
+
+h1.innerHTML = "<h1>AWADH is the champion</h1>";
